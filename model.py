a338484d350a1ea0038727792cdcfc2abee291d1
JJW MBP <jeff.j.waldner@gmail.com>
2018-07-03 16:31:32 +0000

    Train1, Train2; LeNet w/ 10-20 depth.

--- model.py
+++ model.py
@@ -2,27 +2,35 @@ import csv
 import cv2
 import numpy as np
 
-lines = []
-with open('data/Train1_log.csv') as csvfile:
-	reader = csv.reader(csvfile)
-	for line in reader: 
-		lines.append(line)
 
 images = []
 measurements = []
-steer_correct = [0.2, 0, -0.2]
-for line in lines:
-	for i in range(3):
-		sourcepath = line[i]
-		filename = sourcepath.split('/')[-1]
-		currentpath = 'data/Train1/' + filename
-		image = cv2.imread(currentpath)
-		images.append(image)
-		images.append(cv2.flip(image,1)) #Flip image
+datasets = ['Train1', 'Train2']
+
+for dataset in datasets:
+
+	lines = []
+	with open('data/' + dataset + '_log.csv') as csvfile:
+		reader = csv.reader(csvfile)
+		for line in reader: 
+			lines.append(line)
+
+
+	steer_correct = [0.2, 0, -0.2]
+	for line in lines:
+		for i in range(3):
+			sourcepath = line[i]
+			filename = sourcepath.split('/')[-1]
+			currentpath = 'data/' + dataset + '/' + filename
+			image = cv2.imread(currentpath)
+			images.append(image)
+			images.append(cv2.flip(image,1)) #Flip image
 	
-		measurement = float(line[3]) + steer_correct[i]
-		measurements.append(measurement)
-		measurements.append(-measurement)
+			measurement = float(line[3]) + steer_correct[i]
+			measurements.append(measurement)
+			measurements.append(-measurement)
+
+
 
 X_train = np.array(images)
 y_train = np.array(measurements)
@@ -33,16 +41,22 @@ print(X_train.shape)
 
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Cropping2D, Convolution2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Convolution2D, MaxPooling2D
 
 model = Sequential()
 model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160, 320, 3)))
 model.add(Lambda(lambda x:(x/255.0)-0.5))
+model.add(Convolution2D(10, 5, 5, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Convolution2D(20, 5, 5, activation='relu'))
+model.add(MaxPooling2D())
 model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss = 'mse', optimizer = 'adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=1)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=1, batch_size=32)
 
 model.save('model.h5')
 
