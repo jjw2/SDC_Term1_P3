47faf32162a0fbc3db8968780bb10fd1225d7ac0
JJW MBP <jeff.j.waldner@gmail.com>
2018-07-03 15:08:54 +0000

    Added cropping and normalization.

--- model.py
+++ model.py
@@ -29,10 +29,12 @@ print(X_train.shape)
 
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Convolution2D
 
 model = Sequential()
-model.add(Flatten(input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160, 320, 3)))
+model.add(Lambda(lambda x:(x/255.0)-0.5))
+model.add(Flatten())
 model.add(Dense(1))
 
 model.compile(loss = 'mse', optimizer = 'adam')
